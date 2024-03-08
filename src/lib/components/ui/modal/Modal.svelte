0ec8fba1d92ccbf31aaf1008c1dd81d70fabc7e3
abdelhalim <rioman19@gmail.com>
2024-03-08 11:55:01 -0500

    fix backdrop transition

--- src/lib/components/ui/modal/Modal.svelte
+++ src/lib/components/ui/modal/Modal.svelte
@@ -35,13 +35,10 @@
 	<div class="rift-modal" use:portal={root_context.overlayElement}>
 		<div
 			class="w-full h-full flex items-center justify-center fixed inset-0 backdrop-blur-sm z-[-1] bg-black/25 transition-colors duration-100 pointer-events-auto"
+			transition:fade={{ duration: 200 }}
 			onclick={close}
 		>
-			<div
-				class={cn('rift-modal-inner', klass)}
-				transition:fade={{ duration: 100 }}
-				onclick={(ev) => ev.stopPropagation()}
-			>
+			<div class={cn('rift-modal-inner', klass)} onclick={(ev) => ev.stopPropagation()}>
 				<slot />
 			</div>
 		</div>
